4748d51b741e8b5de920dc71a4a7a44b2b067949
Mathias Roustan <roustan_mathias@outlook.fr>
2022-11-23 19:46:11 +0100

    Server implementation

--- PongServer.ts
+++ PongServer.ts
@@ -1,22 +1,50 @@
-import { NewBallState } from "./Ball";
-import { Pong } from "./Pong";
+import { WebSocket } from 'ws';
+
+import { NewBallState } from './Ball';
+import { Pong } from './Pong';
+
+import { PongAction, WsNamespace, WsPongBounce, WsPongReset } from '../../websockets/types';
 
 export class PongServer {
-    private gameState: Pong;
-    
-    constructor() {
-        this.gameState = new Pong(8);
-        
-        this.gameState.onBallBounce((nbs: NewBallState) => {
-           console.log(nbs); 
-        });
-        this.gameState.onBallOut((bo) => {
-           console.log(bo);
-           this.gameState.reset(bo.player1Score, bo.player2Score); 
-        });
-    }
-    
-    update(dt: number) {
-        this.gameState.update(dt);
-    }
+	private readonly gameState: Pong = new Pong(8);
+	private spectators_ws: Array<WebSocket> = [];
+
+	constructor(player1_ws: WebSocket, player2_ws: WebSocket, spectators_ws: Array<WebSocket>) {
+		this.spectators_ws = spectators_ws;
+
+		this.gameState.onBallBounce((nbs: NewBallState) => {
+			const data = JSON.stringify({
+				namespace: WsNamespace.Pong,
+				action: PongAction.Bounce,
+				data: nbs,
+			} as WsPongBounce);
+
+			player1_ws.send(data);
+			player2_ws.send(data);
+			this.dispatchToSpectators(data);
+		});
+		this.gameState.onBallOut((bo) => {
+			const data = JSON.stringify({
+				namespace: WsNamespace.Pong,
+				action: 'RESET',
+				data: bo,
+			} as WsPongReset);
+
+			player1_ws.send(data);
+			player2_ws.send(data);
+			this.dispatchToSpectators(data);
+
+			this.gameState.reset(bo.player1Score, bo.player2Score);
+		});
+	}
+
+	dispatchToSpectators(data: string) {
+		for (const spectator of this.spectators_ws) {
+			spectator.send(data);
+		}
+	}
+
+	update(dt: number) {
+		this.gameState.update(dt);
+	}
 }
